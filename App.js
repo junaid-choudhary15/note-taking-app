099c6c7f4909b039325d9d8994e37e4d1fd0f74c
junaidhd <muhammadjunaidzahidhsp@gmail.com>
2021-01-09 22:14:34 +0500

    screens

--- App.js
+++ App.js
@@ -1,21 +1,24 @@
-import { StatusBar } from "expo-status-bar";
 import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, View, NavigatorIOS } from "react-native";
+import Main from "./app/components/Main";
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>noteeeeee</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+export default class App extends React.Component {
+  render() {
+    return (
+      <NavigatorIOS
+        style={styles.container}
+        initialRoute={{
+          title: "Foxes sound very weird",
+          component: Main,
+        }}
+      />
+    );
+  }
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
+    backgroundColor: "#111111",
   },
 });
